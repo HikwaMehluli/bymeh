4156ab98258613c36f12ce014504668ded9f744c
Mehluli Hikwa <mehlulihikwa@gmail.com>
2022-07-12 02:06:29 +0200

    Add  &copy;  right tag to HTML & JS

--- js/_main.js
+++ js/_main.js
@@ -1,1 +1,10 @@
-console.log("thatAfro");
+// 
+// Copyright Date
+// 
+// This will include it in the console as well
+let year = (new Date()).getFullYear()
+document.getElementById("copyright-date").innerHTML = year
+
+// thatAfro, copyright date on console
+console.log("%c thatAfro", "font-weight: bold; font-size: 20px; color: #008ede");
+console.log("  © Mehluli Hikwa" + ", 1986 - " + year);
